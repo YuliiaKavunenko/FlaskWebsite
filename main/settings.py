d53350f241b43ebe2244d123109dc92e7f49d5b4
LitvinenkoKristina1 <ergo20071229@gamil.com>
2024-10-14 18:08:55 +0300

    send email by kristina

--- main/settings.py
+++ main/settings.py
@@ -1,4 +1,5 @@
-import flask, os, flask_sqlalchemy, flask_migrate
+import flask, os, flask_sqlalchemy, flask_migrate, flask_mail
+from flask_mail import Mail
 
 project = flask.Flask(
     import_name = "main",
@@ -6,7 +7,16 @@ project = flask.Flask(
     instance_path = os.path.abspath(__file__ + "../../instance/data.db")
 )
 
-project.config["SQLALCHEMY_DATABASE_URL"] = "sqlite:///data.db"
+project.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 
 db = flask_sqlalchemy.SQLAlchemy(app = project)
 migrate = flask_migrate.Migrate(app = project, db = db)
+
+project.config["MAIL_SERVER"] = "kavunenko0911@gmail.com"
+project.config["MAIL_PORT"] = 587
+project.config["MAIL_USE_TLS"] = True
+project.config["MAIL_USE_SSL"] = False
+project.config["MAIL_USERNAME"] = "kavunenko0911@gmail.com"
+project.config["MAIL_PASSWORD"] = "kbkg zrxf crkv uqcv"
+
+mail = Mail(project)
