1da227b3a7570c45bfd2a9588f473f934fbe3ca1
LitvinenkoKristina1 <ergo20071229@gamil.com>
2024-10-17 20:55:05 +0300

    update home.app

--- home/views.py
+++ home/views.py
@@ -3,9 +3,9 @@ from main.settings import mail
 from flask_mail import Message
 
 def render_home():
-    user_review = flask.request.form.get("user-review")
     if flask.request.method == "POST":
-        if user_review:
+        error = False
+        if flask.request.form.get("name"):
             name = flask.request.form.get("name")
             print(name)
             email = flask.request.form.get("email")
@@ -15,11 +15,13 @@ def render_home():
             try:
                 msg = Message(
                     subject = "новий відгук",
-                    recipients = "crazycel.hihi@gmail.com",
+                    recipients = ["crazycel.hihi@gmail.com"],
                     body = f"клієнт {name} залишив/ла відгук:\n{review}\nпошта для зворотнього зв'язку {email}",
                     sender = "kavunenko0911@gmail.com"
                 )
                 mail.send(msg)
+                error = False
             except Exception as e:
                 print(f"Помилка: {e}")
-    return flask.render_template(template_name_or_list = "home.html")
+                error = True
+    return flask.render_template(template_name_or_list = "home.html", error = error)
